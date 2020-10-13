f8b5eb74ba000198d2d118b3383ea27b7716ea30
Julia <juliana-zh@yandex.ru>
2020-10-13 17:45:52 +0300

    pq advanced

--- sorter.cpp
+++ sorter.cpp
@@ -20,29 +20,29 @@ void Sorter::ReadDivideSort() {
         throw std::runtime_error("Failed to open file: " + InFileName);
     }
 
-    std::string line;
-    int curSizeBytes = 0;
+    std::string line;    
     std::vector<std::string> buffer;
+    int curSizeBytes = CountCurByteSize(buffer);
+    std::cout << "curSizeBytes: " << curSizeBytes << std::endl;
     buffer.reserve(25000);    
     while (!ifs.eof()) { 
-        getline(ifs, line);        
-        curSizeBytes += line.size();
+        getline(ifs, line);                      
         buffer.push_back(line); 
+        curSizeBytes = CountCurByteSize(buffer);
         if (curSizeBytes >= BufferLimitBytes) {
             std::sort(buffer.begin(), buffer.end());
             PutToFile(buffer);
-            ++MaxCounterFileCreated;
-            curSizeBytes = 0;
-            buffer.clear();
+            ++MaxCounterFileCreated;            
+            buffer.clear();            
         }					
 	}
-    if (curSizeBytes > 0) {
+    if (!buffer.empty()) {
         PutToFile(buffer); 
         ++MaxCounterFileCreated;       
     }
 }
 
-void Sorter::PutToFile(const std::vector<std::string>& buffer) {
+void Sorter::PutToFile(const std::vector<std::string>& buffer) {    
     std::filesystem::path workdir = std::filesystem::current_path();
     std::filesystem::create_directory(TMPDIR);
     std::filesystem::path filename = workdir / TMPDIR / std::to_string(MaxCounterFileCreated);     
@@ -50,9 +50,13 @@ void Sorter::PutToFile(const std::vector<std::string>& buffer) {
     if (!ofs.is_open()) {
         throw std::runtime_error("Failed to open file: " + std::string(filename));
     }  
-    for (int i = 0; i < (int)buffer.size(); ++i) {
+    if (buffer.empty()) {
+        return;
+    }
+    for (int i = 0; i < (int)buffer.size() - 1; ++i) {
         ofs << buffer[i] << std::endl;
     }
+    ofs << buffer.back();
 }
 
 void Sorter::FillPriorityQueue() {    
@@ -63,26 +67,46 @@ void Sorter::FillPriorityQueue() {
         el.ifs = std::make_shared<std::ifstream>(filename);
         if (!el.ifs->is_open()) {
             throw std::runtime_error("Failed to open file: " + std::string(filename));
-        }
-        getline(*(el.ifs), el.line);  
+        }        
+        getline(*(el.ifs), el.line);   
         LineToIfs.push(el);        
     }
 }
 
 void Sorter::MergeWrite() {
     std::ofstream ofs(OutFileName);
-    if (!ifs.is_open()) {
+    if (!ofs.is_open()) {
         throw std::runtime_error("Failed to open file: " + OutFileName);
     }
     while (!LineToIfs.empty()) {
         Elem el = LineToIfs.top();
+        std::cout << "|" << el.line << "|" << std::endl;
         ofs << el.line;
+        std::cout << "size: " << LineToIfs.size() << std::endl;
         if (LineToIfs.size() != 1) {
             ofs << "\n";
-        }      
-       
+        }  
 
+        if (el.ifs->eof()) {
+            LineToIfs.pop();
+            continue;
+        }
+  
+        Elem newEl;
+        newEl.ifs = el.ifs;
+        getline(*(newEl.ifs), newEl.line);
+        LineToIfs.pop();
+        LineToIfs.push(newEl);
     }
+}
+
+void Sorter::CountCurByteSize(const std::vector<std::string>& v) {
+    int size = 0;
+    size += sizeof(std::vector<std::string>) + sizeof(std::string) * vector::size();
+    for(int i = 0; i < (int)v.size(); ++i) {
+        size += v[i].length();
+    }
+    return size;
 }
 
 }
--- sorter.h
+++ sorter.h
@@ -11,7 +11,7 @@ struct Elem {
 	std::string line;
 	std::shared_ptr<std::ifstream> ifs;
     bool operator< (const Elem& b) const {
-        return line < b.line;
+        return line > b.line;
     }
 };
 
@@ -30,9 +30,11 @@ private:
     void FillPriorityQueue();
     void PutToFile(const std::vector<std::string>& buffer);
     void MergeWrite();
+    void CountCurByteSize(const std::vector<std::string>& v);
 private:
-    std::string LargeFileName;
+    std::string InFileName;
     int BufferLimitBytes = 10 * 1024 * 1024;
+    std::string OutFileName;
     int MaxCounterFileCreated = 0;
     const std::string TMPDIR = "tmpdir_sorter";    
     std::priority_queue<Elem> LineToIfs;
